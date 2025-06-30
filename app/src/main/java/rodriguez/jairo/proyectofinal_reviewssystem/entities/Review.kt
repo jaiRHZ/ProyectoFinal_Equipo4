93988fe12e0a6cc02e7c415f54878255c2d29b93
polpoliester <pol_poliester@hotmail.com>
2025-06-30 02:54:46 -0700

    Login & SignUp en firebase, cambios UI

---
+++ app/src/main/java/rodriguez/jairo/proyectofinal_reviewssystem/entities/Review.kt
@@ -0,0 +1,3 @@
+package rodriguez.jairo.proyectofinal_reviewssystem.entities
+
+data class Review(var rating: Int, var description: String, var fullReview: String, var share: Boolean)
---
+++ app/src/main/java/rodriguez/jairo/proyectofinal_reviewssystem/entities/User.kt
@@ -0,0 +1,4 @@
+package rodriguez.jairo.proyectofinal_reviewssystem.entities
+
+data class User(var name: String, var email: String, var gender: String,
+                var password: String)
